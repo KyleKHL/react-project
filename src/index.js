eab5b12234fd158105f0260bec0dec09e768ba4f
Kyle Lau <kyle.lau15@gmail.com>
2023-06-06 18:30:31 -0400

    dev branch init commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const BookList = ()=>{
+    return(
+        <section>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    )
+}
+
 const Book = () => {
   return (
     <article>
-        <div>
-            <p>HELLO WORLD!</p>
-        </div>
+        <Image/>
+        <Title/>
+        <Author/>
     </article>
   )
 }
 
+const Image = () => <h2>Image placeholder</h2>
+const Title = () => <h2>Book Title</h2>
+const Author = () => {
+    return <h4>Author</h4>
+}
 
 export default Book
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Book/>)
+root.render(<Booklist />)
