9a6a29ce44042d9302630eeaae95247ba29adb17
Kyle Lau <kyle.lau15@gmail.com>
2023-06-06 16:59:19 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Book = () => {
+  return (
+    <article>
+        <div>
+            <p>HELLO WORLD!</p>
+        </div>
+    </article>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+export default Book
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<Book/>)
